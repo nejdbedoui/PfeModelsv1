85326901bd900688a07765b4f31514ac08d19b8f
nejdbedoui <nejdbedoui@gmail.com>
2022-04-29 20:31:30 +0100

    add image

---
+++ src/main/java/com/bprice/persistance/model/Storage.java
@@ -0,0 +1,22 @@
+package com.bprice.persistance.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Lob;
+import java.io.InputStream;
+
+@Data
+@Document
+public class Storage {
+    @Id
+    private String idStorage;
+    private String name;
+    private String type;
+    private long size;
+    @Lob
+    private byte[] imagedata;
+
+    private InputStream videodata;
+}
